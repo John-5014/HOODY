3fdd29e37781e67a257105c0b2cd5ec57d92ae25
John-5014 <kaiserjohn52@gmail.com>
2019-12-01 09:16:30 +0300

    adds profile and hood models with signals instances

--- micasa/models.py
+++ micasa/models.py
@@ -43,7 +43,64 @@ class Category(models.Model):
 
 
 
+class Hood(models.Model):
+    name = models.CharField(max_length=50)
+    image = ImageField()
+    occupants = models.CharField(max_length=50)
+    location = models.ForeignKey(Location)
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
 
+
+
+
+    class Meta:
+        ordering = ['-pk']
+
+    def save_hood(self):
+        self.save()
+
+
+    def delete_hood(self):
+        self.delete()
+
+
+    @classmethod
+    def search_hood(cls, search_term):
+        hood = Hood.objects.filter(name__icontains=search_term)
+        return hood
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to = 'profile/',blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    bio = models.CharField(max_length = 255,null = True)
+    full_name = models.CharField(max_length=255, null=True)
+    hood = models.ForeignKey(Hood,null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def save_profile(self):
+        self.save()
+
+
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 
 
