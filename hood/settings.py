a308567a6163008e85a68969bbfdf7dbe442429d
John-5014 <kaiserjohn52@gmail.com>
2019-12-02 14:59:40 +0300

    initial commit for app

--- hood/settings.py
+++ hood/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 # import dj_database_url
 # from decouple import config,Csv
 
@@ -161,4 +162,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+
+django_heroku.settings(locals())
 
